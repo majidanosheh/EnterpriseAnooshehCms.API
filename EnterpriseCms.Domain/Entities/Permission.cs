3fe702a392a8eba28d5760388191eaab4e8726a6
majidanosheh <anoshehmajid@gmail.com>
2025-11-14 02:29:44 +0330

    Add Permission, RolePermission, and Tenant entities for core CMS

---
+++ EnterpriseCms.Domain/Entities/Permission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnterpriseCms.Domain.Entities
+{
+    public class Permission
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Resource { get; set; }
+        public string Action { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public virtual ICollection<RolePermission> RolePermissions { get; set; } = new List<RolePermission>();
+    }
+}
---
+++ EnterpriseCms.Domain/Entities/Tenant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnterpriseCms.Domain.Entities
+{
+    public class Tenant
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
